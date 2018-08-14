3c42375f057ef6a8777a4d5f589c0bb2892b0382
27kUltraHd <nhaison@pdx.edu>
2018-08-13 23:34:45 -0700

    added server.js

--- javascript/scrape.js
+++ javascript/scrape.js
@@ -1,18 +1,38 @@
-//require('dotenv').config();
+// require('dotenv').config();
 
 const snoowrap = require('snoowrap');
-const jQuery = require('jquery');
+const moment = require('moment');
+const express = require('express');
+
+var server = express();
+
+server.get('/', (req, res)=> {
+  res.set('Content-Type', 'text/html');
+  res.status(200).send("<!DOCTYPE html><html><body>Welcome</body></html>");
+})
+server.get('/top', (req, res) => {
+
+  res.set('Content-Type', 'text/html');
+  res.status(200).send("<!DOCTYPE html><html><body>lorem ipsom</body></html>");
+});
+
+server.listen(process.env.PORT || 8080);
+
+
+
+
+
+
+
+
+
+
+
+
 
 // anytime you request ANYTHING from reddit please use await
 async function run(){
-  // Build Snoowrap and Snoostorm clients
-// const r = new snoowrap({
-//   userAgent: 'scraper',
-//   clientId: process.env.CLIENT_ID,
-//   clientSecret: process.env.CLIENT_SECRET,
-//   username: process.env.REDDIT_USER,
-//   password: process.env.REDDIT_PASS
-// });
+
 const r = new snoowrap({
   userAgent: 'scraper',
   clientId: "BwU0WnuK8201AQ",
@@ -21,24 +41,37 @@ const r = new snoowrap({
   password: "haihai123"
 });
 
+// Grabs subreddit by name
 const sub = r.getSubreddit('funny');
 // Grabs top posts
 const top = await sub.getTop();
 // Grabs first post
 const first = top[0];
-console.log(first.title);
-console.log(first.ups);
-console.log(first.permalink);
-console.log(first.url);
-console.log(first.created);
-console.log(first.created_utc);
-console.log(first.author);
 
-jQuery("#title").text(first.title);
+var body = {};
 
-//r.getTop('AskReddit').then(console.log);
+body.title = first.title;
+body.ups = first.ups;
+body.permalink = first.permalink;
+body.url = first.url;
+body.created_utc = first.created_utc;
+body.author = first.author.name;
+body.moment = moment.unix(first.created_utc).format('MMMM Do YYYY, h:mm:ss a');
+
+var jzon = JSON.stringify( body);
+console.log(body);
+console.log("------------");
+
 }
 
-run().catch(console.error);
+// run().catch(console.error);
 
-jQuery("#submit").on("click", run());
+
+ // Build Snoowrap and client
+// const r = new snoowrap({
+//   userAgent: 'scraper',
+//   clientId: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+//   username: process.env.REDDIT_USER,
+//   password: process.env.REDDIT_PASS
+// });
---
+++ javascript/server.js
@@ -0,0 +1,1 @@
+// handlers server side of sockets
