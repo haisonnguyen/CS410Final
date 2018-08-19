3f0b2091cbb16933de25a49687105e2a1c2657db
Haison Nguyen <hat@Haisons-MacBook-Pro.local>
2018-08-18 20:36:57 -0700

    updated templates

--- javascript/scrape.js
+++ javascript/scrape.js
@@ -8,59 +8,84 @@ module.exports = {
   run: run
 }
 
-async function run(desired){
+async function run(desired) {
 
-const r = new snoowrap({
-  userAgent: 'scraper',
-  clientId: "BwU0WnuK8201AQ",
-  clientSecret: "hwttan89eHOYV3w8qaeZ2Me10Ic",
-  username: "FlimsyInterest",
-  password: "haihai123"
-});
+  const r = new snoowrap({
+    userAgent: 'scraper',
+    clientId: "BwU0WnuK8201AQ",
+    clientSecret: "hwttan89eHOYV3w8qaeZ2Me10Ic",
+    username: "FlimsyInterest",
+    password: "haihai123"
+  });
 
-// Grabs subreddit by name
-const sub = r.getSubreddit('funny');
-var result;
+  // Grabs subreddit by name
+  const sub = r.getSubreddit('funny');
+  var result;
 
-if(desired == "top"){
-// Grabs top posts
-  result = await sub.getTop();
+  if (desired == "top") {
+    // Grabs top posts
+    result = await sub.getTop({ limit: 3 });
+  }
+  else if (desired == "hot") {
+    result = await sub.getHot({ limit: 3 });
+  }
+  else if (desired == "new") {
+    result = await sub.getNew({ limit: 3 });
+
+  }
+  else if (desired == "controversial") {
+    result = await sub.getControversial({ limit: 3 });
+
+  }
+  else if (desired == "rising") {
+    result = await sub.getRising({ limit: 3 });
+  }
+  else
+    return;
+
+  // Grabs first post
+  const first = result[0];
+  var array = [];
+  var body;
+
+  for (var i = 0; i < result.length; ++i) {
+    // if it is a video
+    if (result[i].media) {
+      post.vidurl = result[i].media.reddit_video.fallback_url;
+    }
+    var post = {};
+
+    post.title = result[i].title;
+    post.ups = result[i].ups;
+    post.permalink = result[i].permalink;
+    post.url = result[i].url;
+    post.created_utc = result[i].created_utc;
+    post.author = "/u/" + result[i].author.name;
+    post.moment = moment.unix(result[i].created_utc).format('MMMM Do YYYY, h:mm:ss a');
+    post = { "post": post }
+    array.push(post);
+  }
+  // return JSON.stringify(array);
+  return array;
 }
-else if(desired == "hot"){
-  result = await sub.getHot();
-}
-else if(desired == "new"){
-  result = await sub.getNew();
+// //console.log(array);
+// // if it is a video
+// var post = {};
 
-}
-else if(desired =="controversial"){
-  result = await sub.getControversial();
+// if (first.media) {
+//   post.vidurl = first.media.reddit_video.fallback_url;
+// }
+// post.title = first.title;
+// post.ups = first.ups;
+// post.permalink = first.permalink;
+// post.url = first.url;
+// post.created_utc = first.created_utc;
+// post.author = "/u/" + first.author.name;
+// post.moment = moment.unix(first.created_utc).format('MMMM Do YYYY, h:mm:ss a');
+// body = { "post": post }
+// var jzon = JSON.stringify(body);
+// return;
 
-}
-else if(desired == "rising"){
-  result = await sub.getRising();
-}
-else 
-  return ;
-
-// Grabs first post
-const first = result[0];
-var body = {};
-
-if(first.media) {
-  body.vidurl = first.media.reddit_video.fallback_url;
-}
-
-body.title = first.title;
-body.ups = first.ups;
-body.permalink = first.permalink;
-body.url = first.url;
-body.created_utc = first.created_utc;
-body.author = "/u/" + first.author.name;
-body.moment = moment.unix(first.created_utc).format('MMMM Do YYYY, h:mm:ss a');
-var jzon = JSON.stringify( body);
-return body
-}
 
 
 
--- javascript/server.js
+++ javascript/server.js
@@ -5,79 +5,108 @@ var bd = require('./scrape.js');
 var mustache = require('mustache');
 var server = express();
 var fs = require('fs');
-var jQuery = require('jquery');
 
 server.use(express.static(__dirname));
 
-var text = "lit";
+
+var template = "lit";
+var postData;
 
 // grabs the "template" from index.html for rendering :)
 fs.readFile("../templates/displaytemplate.html", function (err, html) {
   if (err) console.log(err);
   console.log("Finished reading template");
-  text = html.toString('utf8');
+  template = html.toString('utf8');
+});
+
+fs.readFile("../templates/postTemplate.html", (err, html) => {
+  if (err) console.log(err);
+  console.log("Finished reading template");
+  postData = html.toString('utf8');
+});
+
+server.get("/:param", async (req, res) => {
+  // returns array
+  var str = await bd.run(req.params.param);
+  if (str) {
+    var numEntries = str.length;
+    for (var i = 0; i < numEntries; ++i) {
+      template = template.slice(0, template.indexOf("</ul>")) + postData + template.slice(template.indexOf("</ul>"));
+      console.log(template);
+    }
+
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.write(mustache.render(template, JSON.stringify(str)));
+    res.end();
+  }
+  else {
+    res.writeHeader(400, { "Content-Type": "text/html" });
+    res.write("Not found");
+    res.end();
+  }
 });
 
 server.get('/', (req, res) => {
   res.writeHeader(200, { "Content-Type": "text/html" });
-  res.write(text);
+  res.write(template);
   res.end();
 });
 
-// serve hot
-server.get('/hot', async (req, res) => {
+// // serve hot
+// server.get('/hot', async (req, res) => {
 
-  // grabs object data to display from scrape.js
-  var str = await bd.run(req.url.slice(1));
+//   // grabs object data to display from scrape.js
+//   var str = await bd.run(req.url.slice(1));
 
-  res.set('Content-Type', 'text/html');
-  res.write(mustache.render(text, str));
-  res.end();
-});
+//   res.set('Content-Type', 'text/html');
+//   res.write(mustache.render(template, str));
+//   res.end();
+// });
 
-// serve new
-server.get('/new', async (req, res) => {
+// // serve new
+// server.get('/new', async (req, res) => {
 
-  // grabs object data to display from scrape.js
-  var str = await bd.run(req.url.slice(1));
+//   // grabs object data to display from scrape.js
+//   var str = await bd.run(req.url.slice(1));
 
-  res.set('Content-Type', 'text/html');
-  res.write(mustache.render(text, str));
-  res.end();
-});
+//   res.set('Content-Type', 'text/html');
+//   res.write(mustache.render(template, str));
+//   res.end();
+// });
 
-// serve controversial
-server.get('/controversial', async (req, res) => {
+// // serve controversial
+// server.get('/controversial', async (req, res) => {
 
-  console.log(req.url.slice(1))
-  // grabs object data to display from scrape.js
-  var str = await bd.run(req.url.slice(1));
+//  // console.log(req.url.slice(1))
+//   // grabs object data to display from scrape.js
+//   var str = await bd.run(req.url.slice(1));
 
-  res.set('Content-Type', 'text/html');
-  res.write(mustache.render(text, str));
-  res.end();
-});
+//   res.set('Content-Type', 'text/html');
+//   res.write(mustache.render(template, str));
+//   res.end();
+// });
 
-// serve top
-server.get('/top', async (req, res) => {
+// // serve top
+// server.get('/top', async (req, res) => {
 
-  // grabs object data to display from scrape.js
-  var str = await bd.run(req.url.slice(1));
+//   // grabs object data to display from scrape.js
+//   var str = await bd.run(req.url.slice(1));
 
-  res.set('Content-Type', 'text/html');
-  res.write(mustache.render(text, str));
-  res.end();
-});
+//   res.set('Content-Type', 'text/html');
+//   res.write(mustache.render(template, str));
+//   res.end();
+// });
 
-// serve rising
-server.get('/rising', async (req, res) => {
+// // serve rising
+// server.get('/rising', async (req, res) => {
 
-  // grabs object data to display from scrape.js
-  var str = await bd.run(req.url.slice(1));
+//   // grabs object data to display from scrape.js
+//   var str = await bd.run(req.url.slice(1));
 
-  res.set('Content-Type', 'text/html');
-  res.write(mustache.render(text, str));
-  res.end();
-});
+//   res.set('Content-Type', 'text/html');
+//   res.write(mustache.render(template, str));
+//   res.end();
+// });
 
 server.listen(process.env.PORT || 8080);
+
